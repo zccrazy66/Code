96da523791b09da1ab28730052e4f68292b50993
jungle <1037044430@qq.com>
2020-08-15 17:02:52 +0800

    init

--- bishi/src/main/java/meituan/zczc/Main.java
+++ bishi/src/main/java/meituan/zczc/Main.java
@@ -1,8 +1,6 @@
 package meituan.zczc;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * 不一样的逆序数
@@ -12,8 +10,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int input = in.nextInt();
-        List list = new Main().solution(input);
+        int n = in.nextInt();
+        List list = new Main().solve(n);
         System.out.println(list.size());
         for (int i = 0; i < list.size(); i++) {
             List mid = (List) list.get(i);
@@ -21,14 +19,14 @@ public class Main {
         }
     }
 
-    private List<List<Integer>> solution(int input){
+    private List<List<Integer>> solve(int n){
         List<List<Integer>> lists = new ArrayList<List<Integer>>();
-        for (int i = 1; i < input; i++) {
+        for (int i = 1; i < n; i++) {
             List<Integer> list = new ArrayList<Integer>();
-            if (i * 4 > input){
+            if (i * 4 > n){
                 break;
             }
-            int target = nixu(i);
+            int target = helper(i);
             if (i*4 == target){
                 list.add(i);
                 list.add(target);
@@ -38,7 +36,7 @@ public class Main {
         return lists;
     }
 
-    private int nixu(int i){
+    private int helper(int i){
         int res = 0;
         while (i!=0){
             res = res * 10 + i % 10;
