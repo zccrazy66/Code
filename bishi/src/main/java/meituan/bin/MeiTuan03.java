1e418128ac77b7fa962e80062a19a0be9aa5875a
linsongbin175 <68365593+linsongbin175@users.noreply.github.com>
2020-08-15 19:51:32 +0800

    MeiTuan03

--- bishi/src/main/java/meituan/bin/MeiTuan03.java
+++ bishi/src/main/java/meituan/bin/MeiTuan03.java
@@ -1,11 +1,33 @@
 package meituan.bin;
 
+import meituan.zczc.WorkflowNode;
+
 import java.util.*;
 
 /**
  * Created by 林松斌的船 on 2020/8/15.
  */
 public class MeiTuan03 {
+    int resmax=Integer.MIN_VALUE;
+    public static void main(String args[]) {
+        Scanner cin = new Scanner(System.in);
+        Main main=new Main();
+        while (cin.hasNext()) {
+            WorkflowNode node = WorkflowNode.load(cin.next());
+            main.help(node,node.timeoutMillis);
+        }
+        System.out.println(main.resmax);
+
+
+    }
+    public void help(WorkflowNode workflowNode,int time){
+        if (workflowNode.nextNodes==null){
+            resmax=Math.max(resmax,time);
+        }
+        for (int i=0;i<workflowNode.nextNodes.size();i++){
+            help(workflowNode.nextNodes.get(i),time+workflowNode.nextNodes.get(i).timeoutMillis);
+        }
+    }
     public static void main(String[] args){
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
