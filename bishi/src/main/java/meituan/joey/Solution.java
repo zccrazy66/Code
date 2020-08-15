5fa7d5b71125868b1eb4831f051fe7891b050417
Joey <chenxu2936@gmail.com>
2020-08-15 15:53:00 +0800

    test222

--- bishi/src/main/java/meituan/joey/Solution.java
+++ bishi/src/main/java/meituan/joey/Solution.java
@@ -7,6 +7,7 @@ package meituan.joey;
 public class Solution {
     public static void main(String[] args) {
         System.out.println("testtest");
+        System.out.println("test22222");
     }
 
 }
