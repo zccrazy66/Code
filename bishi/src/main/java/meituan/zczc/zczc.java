11de63eb0ad0dae949959c4b80a4e181ab13d992
zccrazy66 <596034145@qq.com>
2020-08-15 19:41:43 +0800

    zczczc

--- bishi/src/main/java/meituan/zczc/zczc.java
+++ bishi/src/main/java/meituan/zczc/zczc.java
@@ -1,46 +1,11 @@
+package meituan.zczc;
+
+import meituan.zczc.WorkflowNode;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
-class WorkflowNode {
-    String nodeId;
-    int timeoutMillis;
-    List<WorkflowNode> nextNodes;
-    boolean initialised;
 
-    public static WorkflowNode load(String value) {
-        // Create head node;
-        Map<String, WorkflowNode> map = new HashMap<>();
-        WorkflowNode head = new WorkflowNode("HEAD", 0, null);
-        map.put(head.nodeId, head);
-
-        for (String nodeValue : value.split("\\|")) {
-            String[] properties = nodeValue.split("\\`");
-            WorkflowNode node = map.get(properties[0]);
-
-            node.timeoutMillis = Integer.parseInt(properties[1]);
-            node.initialised = true;
-
-            // Check next nodes
-            if (properties[2].equals("END")) {
-                continue;
-            }
-            node.nextNodes = Arrays.stream(properties[2].split(","))
-                    .map(p -> new WorkflowNode(p, 0, null))
-                    .collect(Collectors.toList());
-            node.nextNodes.forEach(p -> map.put(p.nodeId, p));
-
-            map.put(node.nodeId, node);
-        }
-
-        return head;
-    }
-
-    public WorkflowNode(String nodeId, int timeoutMillis, List<WorkflowNode> nextNodes) {
-        this.nodeId = nodeId;
-        this.timeoutMillis = timeoutMillis;
-        this.nextNodes = nextNodes;
-    }
-}
 
 public class zczc
 {
@@ -54,7 +19,16 @@ public class zczc
         }
     }
     private static int solution(WorkflowNode node){
-
+        List<WorkflowNode> list = node.nextNodes;
+        int max = 0;
+        for (int i = 0; i < list.size(); i++) {
+            WorkflowNode workflowNode = list.get(i);
+            max = Math.max(max,workflowNode.timeoutMillis);
+            if (workflowNode.nextNodes != null){
+                max = Math.max(max,solution(workflowNode));
+            }
+        }
+        return max;
     }
 
 
