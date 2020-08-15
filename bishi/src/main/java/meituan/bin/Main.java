2ea0a3d113aa50289f547d8ab8b325161ce1c9d1
linsongbin175 <68365593+linsongbin175@users.noreply.github.com>
2020-08-15 19:46:07 +0800

    Xiecheng02

--- bishi/src/main/java/meituan/bin/Main.java
+++ bishi/src/main/java/meituan/bin/Main.java
@@ -6,47 +6,7 @@ import java.util.stream.Collectors;
 /**
  * Created by 林松斌的船 on 2020/8/15.
  */
- class WorkflowNode {
-    String nodeId;
-    int timeoutMillis;
-    List<WorkflowNode> nextNodes;
-    boolean initialised;
 
-    public static WorkflowNode load(String value) {
-        // Create head node;
-        Map<String, WorkflowNode> map = new HashMap<>();
-        WorkflowNode head = new WorkflowNode("HEAD", 0, null);
-        map.put(head.nodeId, head);
-
-        for (String nodeValue : value.split("\\|")) {
-            String[] properties = nodeValue.split("\\`");
-            WorkflowNode node = map.get(properties[0]);
-
-            node.timeoutMillis = Integer.parseInt(properties[1]);
-            node.initialised = true;
-
-            // Check next nodes
-            if (properties[2].equals("END")) {
-                continue;
-            }
-            node.nextNodes = Arrays.stream(properties[2].split(","))
-                    .map(p -> new WorkflowNode(p, 0, null))
-                    .collect(Collectors.toList());
-            node.nextNodes.forEach(p -> map.put(p.nodeId, p));
-
-            map.put(node.nodeId, node);
-        }
-
-        return head;
-    }
-
-    public WorkflowNode(String nodeId, int timeoutMillis, List<WorkflowNode> nextNodes) {
-        this.nodeId = nodeId;
-        this.timeoutMillis = timeoutMillis;
-        this.nextNodes = nextNodes;
-    }
-
-}
 
 public class Main {
      int resmax=Integer.MIN_VALUE;
