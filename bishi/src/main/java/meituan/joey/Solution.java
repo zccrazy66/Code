fe788fb61a54dcfd11e56276bfbbed92971aa5a2
Joey <chenxu2936@gmail.com>
2020-08-15 15:43:16 +0800

    测试 推送

--- bishi/src/main/java/meituan/joey/Solution.java
+++ bishi/src/main/java/meituan/joey/Solution.java
@@ -1,16 +1,12 @@
 package meituan.joey;
 
-import javax.crypto.spec.PSource;
-
 /**
  * @author JoeyChen
  * @data 2020/8/15 14:36
  */
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("hello zc");
-        System.out.println("hello test");
-        System.out.println("测试提交功能");
+        System.out.println("测试推送");
     }
 
 }
