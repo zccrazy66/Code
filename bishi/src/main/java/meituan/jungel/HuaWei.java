8e14cab07ce04d70c6de7db02433ea4ad3a32be8
jungle <1037044430@qq.com>
2020-08-19 19:58:52 +0800

    init

--- bishi/src/main/java/meituan/jungel/HuaWei.java
+++ bishi/src/main/java/meituan/jungel/HuaWei.java
@@ -7,6 +7,22 @@ public class HuaWei {
         Scanner scanner = new Scanner(System.in);
         String frame = scanner.nextLine();
         String brick = scanner.nextLine();
+        int m = frame.length();
+        int n = brick.length();
+        int[] arr = new int[n];
+        // 如果宽度相等，直接插入
+
+        if (m==n) {
+
+        }
+
+        // 找到最新接触的列
+        for (int i = 0; i < n; i++) {
+
+            int value = Integer.parseInt("" + frame.charAt(i)) + Integer.parseInt("" + brick.charAt(i));
+
+
+        }
 
 
     }
