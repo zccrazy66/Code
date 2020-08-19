92e7e90ed762b872e60f2d58fc6c0e59d763aa46
jungle <1037044430@qq.com>
2020-08-19 19:23:52 +0800

    init

---
+++ bishi/src/main/java/meituan/jungel/HuaWei.java
@@ -0,0 +1,13 @@
+package meituan.jungel;
+
+import java.util.Scanner;
+
+public class HuaWei {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String frame = scanner.nextLine();
+        String brick = scanner.nextLine();
+
+
+    }
+}
