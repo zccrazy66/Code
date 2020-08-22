b6fa721e422e5d442162defe3665f4270c8f7c28
jungle <1037044430@qq.com>
2020-08-22 18:17:52 +0800

    init

--- bishi/src/main/java/笔试/jungel/code.java
+++ bishi/src/main/java/笔试/jungel/code.java
@@ -1,7 +1,8 @@
-package main.java.笔试.jungel;
+package 笔试.jungel;
 
 public class code {
     public static void main(String[] args) {
+        System.out.println("test");
         System.out.println("test");
 
     }
