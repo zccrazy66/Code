65024fcfcd81d5e05644000a9a74283567a45284
zccrazy66 <596034145@qq.com>
2020-08-23 21:31:41 +0800

    tx03

--- bishi/src/main/java/笔试/zczc/腾讯/Main.java
+++ bishi/src/main/java/笔试/zczc/腾讯/Main.java
@@ -22,37 +22,36 @@ public class Main {
         }
     }
 
-
-
-    public static int minCut(String s) {
-        int n = s.length();
-        char[] arr = s.toCharArray();
-        int[] dp = new int[n+1];
-        dp[0] = -1;
-        boolean[][] isSymmetry = getSymmetry(arr);
-        for (int i=1; i<n; i++) {
-            dp[i+1] = 1 + dp[i];
-            for (int j=i-1; j>=0; j--)
-                if (isSymmetry[j][i]) {
-                    dp[i+1] = Math.min(dp[i+1], dp[j] + 1);
+        public static int minCut(String s){
+            int n = s.length();
+            boolean[][] check = new boolean[n + 1][n + 1];
+            int[] dp = new int[n + 1];
+            char[] cs = s.toCharArray();
+            int j;
+            //初始化， 把所有是回文串的放入check 数组， 根据i和j就能判断子串s[i,j]是否回文串
+            for(int len = 1; len <= n; len++){
+                for(int i = 1; i + len - 1 <= n; i++){
+                    j = i + len - 1;
+                    if(len == 1)    {
+                        check[i][j] = true;
+                    }else{
+                        if(cs[i - 1] == cs[j - 1]) {
+                            check[i][j] = len == 2 ? true: check[i + 1][j - 1];
+                        }
+                    }
                 }
-        }
-        return dp[n] + 1;
-    }
-
-    static boolean[][]  getSymmetry(char[] arr) {
-        int n = arr.length;
-        boolean[][] ans = new boolean[n][n];
-        for (int k=0; k<2*n-3; k++) {
-            int left = k / 2;
-            int right = (k & 1) == 0 ? left + 1 : left + 2;
-            while (left >=0 && right < n) {
-                if (arr[left] == arr[right]) ans[left][right] = true;
-                else break;
-                left--;
-                right++;
             }
+            dp[0] = -1;
+            for(int i = 1; i <= n; i++){
+                dp[i] = dp[i - 1] + 1;
+                for(int k = 1; k < i; k++){
+                    if(check[k][i]){
+                        dp[i] = Math.min(dp[i], dp[k - 1] + 1);
+                    }
+                }
+            }
+            return dp[n] + 1;
         }
-        return ans;
-    }
+
+
 }
