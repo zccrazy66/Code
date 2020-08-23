70d678a2ab66ef661b1b637bb528dbae7f504952
zccrazy66 <596034145@qq.com>
2020-08-23 21:03:03 +0800

    tx04

---
+++ bishi/src/main/java/笔试/zczc/腾讯/Main.java
@@ -0,0 +1,64 @@
+package main.java.笔试.zczc.腾讯;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        int Q = in.nextInt();
+        List list = new ArrayList();
+        while (Q > 0){
+            int l = in.nextInt();
+            int r = in.nextInt();
+            System.out.println(s.substring(l - 1,r));
+            list.add(minCut(s.substring(l,r)));
+            Q--;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+
+
+    public static int minCut(String s) {
+        int len = s.length();
+        // 特判
+        if (len < 2) {
+            return 0;
+        }
+
+        int[] dp = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            dp[i] = i;
+        }
+
+        boolean[][] checkPalindrome = new boolean[len][len];
+        for (int right = 0; right < len; right++) {
+            for (int left = 0; left <= right; left++) {
+                if (s.charAt(left) == s.charAt(right) && (right - left <= 2 || checkPalindrome[left + 1][right - 1])) {
+                    checkPalindrome[left][right] = true;
+                }
+            }
+        }
+
+        for (int i = 1; i < len; i++) {
+            if (checkPalindrome[0][i]){
+                dp[i] = 0;
+                continue;
+            }
+
+            for (int j = 0; j < i; j++) {
+                if (checkPalindrome[j + 1][i]) {
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp[len - 1] + 1;
+    }
+}
