d6995825f52bbd467f5ca5301dc178387dbbfb99
jungle <1037044430@qq.com>
2020-09-05 16:29:07 +0800

    commit

--- bishi/src/main/java/笔试/jungel/zhongxing/First.java
+++ bishi/src/main/java/笔试/jungel/zhongxing/First.java
@@ -2,47 +2,76 @@ package 笔试.jungel.zhongxing;
 
 import com.alibaba.fastjson.JSON;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
+// 7
+// 5 3 2 1 6 7 4
 public class First {
+    private static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private static int maxLen;
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        if (n<3||m<3) {
-            System.out.println(0);
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        if (n==1) {
+            System.out.println(1);
             return;
         }
-        scanner.nextLine();
-        int[][] grid = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                grid[i][j]=scanner.nextInt();
-            }
+        TreeNode root = new TreeNode(arr[0], null, null);
+        for (int i = 1; i < n; i++) {
+
+            TreeNode tree = getTree(root, arr[i]);
+            root = tree;
         }
 
-        int max = new First().getMax(grid);
-        System.out.println(max);
-
+        getLen(root);
+        System.out.println(maxLen);
 
     }
 
+    private static int getLen(TreeNode root) {
 
-    private int getMax(int[][] grid) {
+        if (root==null) return 0;
+        if (root.left==null&&root.right==null) return 1;
+        int left = getLen(root.left);
+        int right = getLen(root.right);
+        int temp = left + right+1;
+        maxLen = Math.max(temp,maxLen);
+        return Math.max(left,right)+1;
+    }
 
-        int m = grid.length;
-        int n = grid.length;
+    /**
+     * 顺序构造二叉排序树
+     */
+    private static TreeNode getTree(TreeNode root,int target) {
 
-        int max = 0;
-        for (int i = 1; i < m - 1; i++) {
-            for (int j = 1; j < n - 1; j++) {
-
-                int res = grid[i][j] + grid[i - 1][j] + grid[i + 1][j] + grid[i][j - 1] + grid[i][j + 1];
-                if (res > max) max = res;
-            }
+        if (root==null) {
+            return new TreeNode(target,null,null);
         }
 
-        return max;
+        if (root.val>=target) {
+            root.left = getTree(root.left, target);
+        }else {
+            root.right= getTree(root.right, target);
+        }
+
+        return root;
     }
 }
