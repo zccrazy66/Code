d5b89708a7c6e3286536b8e466130c615b9b6a2d
zccrazy66 <596034145@qq.com>
2020-09-05 16:05:52 +0800

    tx03

--- bishi/src/main/java/笔试/zczc/华为/huawei02.java
+++ bishi/src/main/java/笔试/zczc/华为/huawei02.java
@@ -1,52 +1,47 @@
 package main.java.笔试.zczc.华为;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class huawei02 {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int[] input = new int[N];
-        for (int i = 0; i < N; i++) {
-            input[i] = in.nextInt();
-        }
+//        Scanner in = new Scanner(System.in);
+//        int N = in.nextInt();
+//        int[] input = new int[N];
+//        for (int i = 0; i < N; i++) {
+//            input[i] = in.nextInt();
+//        }
+//        BigDecimal zh = C(5, 2);
+        System.out.println(C(5,2));
 
-        System.out.println(solution(input));
+//        System.out.println(solution(input));
 
     }
 
-    private static int solution(int[] input){
-        int length = input.length;
-        if (length < 1) return 0;
-        Arrays.sort(input);
-        int mid = 1;
-        int res = 1;
-        int count = 0;
-        int zancun = 1;
-        for (int i = 1; i < length; i++) {
-            int temp = 0;
-            if (input[i] != mid){
-                temp = count;
-                mid++;
-            }else {
-                count++;
-            }
-            res = res * (zancun * 2 - temp + 1);
+    public static int A(int n, int m) {
+        int result = 1;
+        // 循环m次,如A(6,2)需要循环2次，6*5
+        for (int i = m; i > 0; i--) {
+            result *= n;
+            n--;// 下一次减一
         }
-        return res;
+        return result;
+    }
+
+    public static int C(int n, int m){
+        // 应用组合数的互补率简化计算量
+        m = m > (n - m) ? (n - m) : m;
+        // 分子的排列数
+        int son = A(n, m);
+        // 分母的排列数
+        int mother = A(m, m);
+        return son / mother ;
     }
 
 
-    private static int CNM(int big,int small){
-        int da = 1;
-        for (int i = 0; i < small; i++) {
-            da  *= big;
-            big--;
-        }
-        return da;
-    }
 
 
 }
