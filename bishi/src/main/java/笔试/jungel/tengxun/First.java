74d14f7c48e9f8d793c715abd2061253533bd81b
jungle <1037044430@qq.com>
2020-09-06 21:26:23 +0800

    commit

---
+++ bishi/src/main/java/笔试/jungel/tengxun/First.java
@@ -0,0 +1,79 @@
+package 笔试.jungel.tengxun;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class First {
+
+
+    private static TreeSet<Integer>[] sets;
+    private static boolean[] isVisted;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        int[] arr = new int[n];
+        isVisted = new boolean[m];
+        TreeSet<Integer>[] people = new TreeSet[m];
+
+        int index_0 = 0;
+        for (int i = 0; i < m; i++) {
+
+            int k= scanner.nextInt();
+
+            people[i]=new TreeSet<>();
+            for (int j = 0; j < k; j++) {
+                int p = scanner.nextInt();
+                if (p==0) index_0=i;
+                people[i].add(p);
+
+            }
+        }
+
+        sets = people;
+        dfs(index_0);
+
+        for (int i = 0; i < m; i++) {
+            if (isVisted[i]) {
+                for (Integer person : people[i]) {
+                    arr[person]++;
+                }
+            }
+        }
+
+
+        int res=0;
+        for (int i : arr) {
+            if (i!=0) res++;
+        }
+
+        System.out.println(res);
+    }
+
+    private static void dfs(int index) {
+
+        isVisted[index]=true;
+        for (Integer integer : sets[index]) {
+
+            List<Integer> nears = getNear(integer);
+
+            for (Integer near : nears) {
+                dfs(near);
+            }
+        }
+    }
+
+
+    private static List<Integer> getNear(int val) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < sets.length; i++) {
+            if (sets[i].contains(val)&&!isVisted[i]) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
