538bbc4ddc1a71391eb8d95141c751a503b7a0ed
Joey <chenxu2936@gmail.com>
2020-09-16 19:28:24 +0800

    种草

--- bishi/src/main/java/笔试/joey/guanglianda/种草.java
+++ bishi/src/main/java/笔试/joey/guanglianda/种草.java
@@ -1,6 +1,8 @@
 package main.java.笔试.joey.guanglianda;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
@@ -13,17 +15,19 @@ public class 种草 {
         int n = scanner.nextInt();
         int m = scanner.nextInt();
         int m_h = scanner.nextInt();
+        //最小堆
         int[] arr = new int[n];
+        Queue<Integer> queue = new PriorityQueue<>();  //最小堆
         for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
+            queue.add(scanner.nextInt());
         }
 
         while (m > 0) {
-            Arrays.sort(arr);
-            arr[0] += m_h;
+            queue.add(queue.poll()+m_h);
             m--;
         }
-        System.out.println(arr[0]);
+
+        System.out.println(queue.peek());
 
     }
 }
