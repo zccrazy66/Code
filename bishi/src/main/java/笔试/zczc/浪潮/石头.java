b8fc4e39d44a8c12b5e08daadbd659fb6d510e86
zccrazy66 <596034145@qq.com>
2020-10-14 21:29:50 +0800

    tx01

--- bishi/src/main/java/笔试/zczc/浪潮/石头.java
+++ bishi/src/main/java/笔试/zczc/浪潮/石头.java
@@ -3,33 +3,21 @@ package main.java.笔试.zczc.浪潮;
 import java.util.Scanner;
 
 public class 石头 {
-        public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
-            int num = 1, max = 1;
-            int tree = in.nextInt();
-            if (tree <= 100000) {
-                int[] c = new int[tree];
-                int[] cc = new int[tree];
-                for (int i = 0; i < tree; i++) {
-                    c[i] = in.nextInt();
-                }
-                if (tree < c.length)
-                    System.out.println(0);
-                for (int i = 0; i < tree; i++) {
-                    cc[i] = c[i];
-                    for (int j = i + 1; j < tree; j++) {
-                        cc[j] = c[j];
-                        if (cc[i] + 1 == cc[j]) {
-                            num += 1;
-                            cc[i] = cc[j];
-                        }
-                    }
-                    if (num > max) {
-                        max = num;
-                    }
-                    num = 1;
-                }
-                System.out.println(tree - max);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextInt()){
+            int m = sc.nextInt();
+            int []arr = new int[m];
+            for(int i = 0; i < m; i ++){
+                arr[i] = sc.nextInt();
             }
+            int[] dp = new int[m + 1];
+            int res = 0;
+            for(int num : arr) {
+                dp[num] = dp[num - 1] + 1;
+                res = Math.max(dp[num], res);
+            }
+            System.out.println(m - res);
         }
+    }
 }
